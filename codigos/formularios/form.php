f98bba78ec7f3011d20a2516cd0d5475d8924144
LAPTOP-UC7AD9SC\Jorge <jorge.moya@mad.vass.es>
2023-05-11 22:29:56 +0200

    Se realizan cambios en el código del formulario.

--- codigos/formularios/form.php
+++ codigos/formularios/form.php
@@ -2,54 +2,50 @@
 <html>
 
 <head>
-    <title>Formulario de Registro</title>
+    <meta charset="UTF-8">
+    <title>Formulario</title>
     <link rel="stylesheet" href="./css/styles.css">
 </head>
 
 <body>
     <h1>Formulario de Registro</h1>
-    <form method="POST" action="registro.php">
+    <form method="POST" action="controllerFormulario.php">
         <div class="form-group">
             <label for="nombre">Nombre:</label>
             <input type="text" id="nombre" name="nombre" required>
-
+        </div>
+        <div class="form-group">
             <label for="apellido">Apellido:</label>
             <input type="text" id="apellido" name="apellido" required>
         </div>
-
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-
-            <label for="genero">Género:</label>
-            <select id="genero" name="genero" required>
-                <option value="">Seleccione una opción</option>
-                <option value="masculino">Masculino</option>
-                <option value="femenino">Femenino</option>
-                <option value="otro">Otro</option>
+            <input type="text" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="telefono">Teléfono:</label>
+            <input type="text" id="telefono" name="telefono" required>
+        </div>
+        <div class="form-group">
+            <label for="pais">País:</label>
+            <select id="pais" name="pais" required>
+                <option value="">Selecciona un país</option>
+                <option value="España">España</option>
+                <option value="México">México</option>
+                <option value="Argentina">Argentina</option>
             </select>
         </div>
         <div class="form-group">
-            <label for="contraseña">Contraseña:</label>
-            <input type="password" id="contraseña" name="contraseña" required>
-
-            <label for="conf_contraseña">Confirmar Contraseña:</label>
-            <input type="password" id="conf_contraseña" name="conf_contraseña" required>
-
-        </div>
-
-
-
-        <div class="form-group">
             <label for="ciudad">Ciudad:</label>
-            <input type="text" id="ciudad" name="ciudad">
-
-            <label for="pais">País:</label>
-            <input type="text" id="pais" name="pais" required>
-
+            <input type="text" id="ciudad" name="ciudad" required>
         </div>
-
-        <input type="submit" value="Registrarse">
+        <div class="form-group">
+            <label for="direccion">Dirección:</label>
+            <textarea id="direccion" name="direccion" required></textarea>
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Enviar Formulario">
+        </div>
     </form>
 </body>
 
